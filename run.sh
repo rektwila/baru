c6e218b4b64991461faad1d9294f1c64f04a9ada
rektw <165019277+rektwila@users.noreply.github.com>
2024-03-26 17:51:53 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-3e633a38-b465-4f53-a2eb-229e0a1b82d0/)
 
 
 echo "$curl_output" >> Update.tsx
